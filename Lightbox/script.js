5b4d5ffdd870da48d6956641284414da8905ebbd
Kabue <mckabue@gmail.com>
2015-09-30 13:38:17 -0700

    Initial Commit

---
+++ Lightbox/script.js
@@ -0,0 +1,23 @@
+﻿
+
+//onclick = "document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'"
+
+(function () {
+    $('#start').click(function (e) {
+        e.preventDefault();
+        $('#light').css('display', 'block'); //css({display: 'block, color: 'red''});
+        $('#fade').css('display', 'block');
+    });
+
+    $('#close').click(function (e) {
+        e.preventDefault();
+        $('#light').css({}).addClass('animate');
+    });
+
+    $("#light").on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function () {
+        $('#fade, #light').css({ 'display': 'none', }).removeClass('animate');
+        //$('#light').css({}).addClass('flashout');
+    });
+
+    //$("#light").on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function (e) { //$(this).off(e);  //$(this).one(e); })
+})($)
